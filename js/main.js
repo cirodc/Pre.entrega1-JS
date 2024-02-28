77d89d899dd8db1a349e0665c13ec42cb569dfe9
cirodc <ciroderobertis7u7@gmail.com>
2024-02-28 11:27:13 -0300

    entrega terminada

--- js/main.js
+++ js/main.js
@@ -12,8 +12,36 @@ const KiloWatts = (0.7355)
 
 function calculo(caballosFuerza, KiloWatts ) {
     return caballosFuerza * KiloWatts
+
 } console.log('En otros numeros, tu ' + ingreseAuto + ' tiene ' + calculo(caballosFuerza, KiloWatts) + ' kW (KiloVatios)' )
 
+
+if (caballosFuerza > 350) {
+    console.log('Podrías modificar un poco tu vehiculo, metele un turbo pa!')
+} else if (caballosFuerza > 550) {
+    console.log('Está muy bien de potencia, con una repro tendrías un auto muy rapido!')
+} else if (caballosFuerza >= 700) {
+    console.log('Fua amigo, alta nave!')
+}
+
+let opcion = prompt('¿Desea realizar el cálculo inverso? (Sí/No)');
+
+function calculoInverso(kilovatios, KiloWatts) {
+    return kilovatios / KiloWatts;
+}
+
+
+if (opcion.toLowerCase() === 'sí' || opcion.toLowerCase() === 'si') {
+    let ingreseKilovatios = prompt('Ingrese los kilovatios para calcular los caballos de fuerza:');
+    let resultadoCaballosFuerza = calculoInverso(ingreseKilovatios, KiloWatts);
+    console.log('En otros números, ' + ingreseKilovatios + ' kW (Kilovatios) equivalen a ' + resultadoCaballosFuerza.toFixed(2) + ' caballos de fuerza.');
+} else {
+    console.log('Gracias por usar la calculadora.');
+}
+
 console.log('¡Gracias por probar nuestra calculadora de CV a kW!')
+
+
+
 console.log('Carpi corregime el trabajo de desarrollo web')
 console.log('Ciro Derobertis Carranza')
